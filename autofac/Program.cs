0e229ddfcfbc7e8d4c5806f4e76fc9ce404dd2da
qjq <jiaqi.qin@sail-fs.com>
2019-03-26 11:29:26 +0800

    添加项目文件。

---
+++ autofac/Program.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autofac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var scope = ContainerService.Container.BeginLifetimeScope())
+            {
+                //解析组件（功能类似于new一个IDateWriter的实现类）
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ autofac/TodayWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autofac
+{
+    public class TodayWriter : IDateWriter
+    {
+        private IOutput _output;//输出日期的类依赖IOutput
+        public TodayWriter(IOutput output) {
+            this._output = output;
+        }
+
+        public void WriteDate() {
+            this._output.Write(DateTime.Today.ToShortDateString());
+        }
+    }
+}
